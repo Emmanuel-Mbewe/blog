a2827a856cb485ff1e87c6ffd5417e1955771868
Emmanuel-Mbewe <emmanuelmbewe180@gmail.com>
2023-07-19 10:37:51 +0200

    Modified the post controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostModule } from './post/post.module';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostModule, PostsModule],
+  imports: [PostsModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
