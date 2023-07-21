fa51ba0fb7ccd7f773762af20c186588053ca32e
Emmanuel-Mbewe <emmanuelmbewe180@gmail.com>
2023-07-21 08:37:56 +0200

    Configured the database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './posts/entities/post.entity';
+import { PostsService } from './posts/posts.service';
+import { PostsController } from './posts/posts.controller';
 
 @Module({
   imports: [
@@ -13,7 +15,8 @@ import { Post } from './posts/entities/post.entity';
       database: 'blog',
       entities: [Post],
       synchronize: true, // set to false in production
-    }),
-  ],
+    }), TypeOrmModule.forFeature([Post]),
+  ],providers: [PostsService],
+  controllers: [PostsController]
 })
 export class AppModule {}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PostsService {
+  constructor(
+    @InjectRepository(Post) private postRepository: Repository<Post>
+  ){}
   create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+    return this.postRepository.save(createPostDto);
   }
 
   findAll() {
-    return `This action returns all posts`;
+    return this.postRepository.find();
   }
 
   findOne(id: number) {
